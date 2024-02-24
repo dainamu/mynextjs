c11772d7ba2cd222b35ec7160c134fd5911d2be5
katsuya-ominami <katsuya.ominami@gmail.com>
2024-02-24 17:05:41 +0900

    tutorial 41 headers

--- route-handler-demo/src/app/profile/api/route.ts
+++ route-handler-demo/src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET(req: Request) {
-  return new Response("Profile api date");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+export async function GET(req: NextRequest) {
+  // 受信リクエストよりヘッダーを読み取る方法
+  const requestHeaders = new Headers(req.headers); // headerオブジェクトを作成するか
+  const headerList = headers(); // headers関数を呼び出す 読み取りせんよう
+  console.log(requestHeaders.get("Authorization"));
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>Profile api date</h1>", {
+    // レスポンスにヘッダーを設定
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
