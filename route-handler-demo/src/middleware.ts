b2f60137f2509e739f6cdcf09463f81a3580a2f5
katsuya-ominami <katsuya.ominami@gmail.com>
2024-02-25 00:43:26 +0900

    tutorial 44 middlewareによるヘッダークッキーの設定

--- route-handler-demo/src/middleware.ts
+++ route-handler-demo/src/middleware.ts
@@ -1,14 +1,24 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const themaPreference = request.cookies.get("thema");
+  console.log(themaPreference);
+  if (!themaPreference) {
+    response.cookies.set("thema", "dark");
+  }
   console.log(request.nextUrl.pathname === "/profile");
   const isprofile = request.nextUrl.pathname === "/profile";
   if (isprofile) {
     // もし/profileにアクセスしたら/helloにリダイレクト
     //return NextResponse.redirect(new URL("/hello", request.url));
-    return NextResponse.rewrite(new URL("/hello", request.url)); // rewriteの場合URLはそのままで/helloの内容がレンダリングされる
+    //return NextResponse.rewrite(new URL("/hello", request.url)); // rewriteの場合URLはそのままで/helloの内容がレンダリングされる
   }
   //   return NextResponse.redirect(new URL("/hello", request.url));
+
+  // ヘッダーの設定
+  response.headers.set("custom-header", "custum-value");
+  return response;
 }
 
 // export const config = {
