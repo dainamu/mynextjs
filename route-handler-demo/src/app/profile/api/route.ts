eac46105c8c73b162afa6b282ab238d85527809f
katsuya-ominami <katsuya.ominami@gmail.com>
2024-02-24 17:45:54 +0900

    tutorial 42 cookies

--- route-handler-demo/src/app/profile/api/route.ts
+++ route-handler-demo/src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 export async function GET(req: NextRequest) {
   // 受信リクエストよりヘッダーを読み取る方法
   const requestHeaders = new Headers(req.headers); // headerオブジェクトを作成するか
@@ -7,10 +7,18 @@ export async function GET(req: NextRequest) {
   console.log(requestHeaders.get("Authorization"));
   console.log(headerList.get("Authorization"));
 
+  const cookieStore = cookies();
+  cookies().set("resultperPage", "20"); // cookiesのsetメソッドでも設定できる
+  cookies().set("thema", "dark");
+  await cookieStore.delete("thema");
+  const thema = req.cookies.get("thema");
+  console.log(thema);
+
   return new Response("<h1>Profile api date</h1>", {
     // レスポンスにヘッダーを設定
     headers: {
       "Content-Type": "text/html",
+      //"Set-Cookie": "thema=dark", // クッキーを設定
     },
   });
 }
