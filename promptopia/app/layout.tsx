df7569676cb952a64a7f194483bf6c741c497de5
katsuya-ominami <katsuya.ominami@gmail.com>
2024-02-25 12:54:14 +0900

    npm run devでエラー

---
+++ promptopia/app/layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "promtopi",
+  description: "Discover & Share AI Prompt",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ promptopia/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HOME() {
+  return <div>HOME</div>;
+}
+
+// const page = () => {
+//   return <div>page</div>;
+// };
+
+// export default page;
