33d31de61abc5560bdf5e747f57081be4a602be8
katsuya-ominami <katsuya.ominami@gmail.com>
2024-02-25 16:22:47 +0900

    commponents 追加

--- promptopia/app/page.tsx
+++ promptopia/app/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default function HOME() {
-  return <div>HOME</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-sm:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is ans open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+    </section>
+  );
 }
 
 // const page = () => {
--- promptopia/tailwind.config.ts
+++ promptopia/tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +7,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       fontFamily: {
         satoshi: ["Satoshi", "sans-serif"],
         inter: ["Inter", "sans-serif"],
@@ -24,5 +18,3 @@ const config: Config = {
   },
   plugins: [],
 };
-
-export default config;
