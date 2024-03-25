c676872bb7ae6d932847c24effaef7f5703fbe67
katsuya-ominami <katsuya.ominami@gmail.com>
2024-03-25 22:27:12 +0900

    useEffectを修正

--- promptopia/components/Nav.tsx
+++ promptopia/components/Nav.tsx
@@ -5,15 +5,18 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
-  const isUserLoggedIn = true;
+  //const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
   useEffect(() => {
-    (async () => {
-      const res: any = await getProviders();
-      setProviders(res);
-    })();
+    const setUpProviders = async () => {
+      const response: any = await getProviders();
+      setProviders(response);
+    };
+    setUpProviders();
   }, []);
+  //alert(session?.user);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -26,10 +29,11 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
+
       <div className="sm:flex hidden">
         {" "}
         {/* sm: はブレークポイント　smの場合min-width 640px　なので639pxなるとhiddenで消える */}
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -65,7 +69,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Image
               src="assets/images/logo.svg"
